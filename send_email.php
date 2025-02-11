aad314b52064b5e913c48f0d2460b947b677e8d6
Emmanuel Fagbemi <martinsogunsina@gmail.com>
2025-02-11 12:14:18 +0100

    new commit

---
+++ send_email.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['comments']);
+
+    // Your Gmail address
+    $to = "emmanueloluwaseun2003@gmail.com";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";                                                                     
+
+    // Email body
+    $email_body = "You have received a new message from your website contact form.\n\n";
+    $email_body .= "Name: $name\n";
+    $email_body .= "Email: $email\n";
+    $email_body .= "Subject: $subject\n";
+    $email_body .= "Message:\n$message\n";
+
+    // Send email
+    if (mail($to, $subject, $email_body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message. Please try again.";
+    }
+} else {
+    echo "Invalid request method.";
+}
+?>
